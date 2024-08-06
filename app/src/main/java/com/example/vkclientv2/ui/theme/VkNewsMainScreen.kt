3134f15725a4d95af84bf2e0224d38632413c629
danielkostetsky <kosteckiy.daniil@wb.ru>
2024-08-06 19:48:16 +0300

    recomposition

--- app/src/main/java/com/example/vkclientv2/ui/theme/VkNewsMainScreen.kt
+++ app/src/main/java/com/example/vkclientv2/ui/theme/VkNewsMainScreen.kt
@@ -1,21 +1,57 @@
 package com.example.vkclientv2.ui.theme
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.res.stringResource
+import kotlinx.coroutines.launch
 
 @Composable
 fun MainScreen() {
+    val snackbarHostState = SnackbarHostState()
+    val scope = rememberCoroutineScope()
+    val fabIsVisible = remember {
+        mutableStateOf(true)
+    }
+
     Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
+        floatingActionButton = {
+            if (fabIsVisible.value) {
+                FloatingActionButton(
+                    onClick = {
+                        scope.launch {
+                            val action = snackbarHostState.showSnackbar(
+                                message = "This is snackbar!",
+                                actionLabel = "Hide FAB",
+                                duration = SnackbarDuration.Long
+                            )
+                            if (action == SnackbarResult.ActionPerformed) {
+                                fabIsVisible.value = false
+                            }
+                        }
+                    }) {
+                    Icon(imageVector = Icons.Filled.Favorite, contentDescription = null)
+                }
+            }
+        },
         bottomBar = {
             NavigationBar {
                 val selectedItemPosition = remember {
@@ -30,7 +66,12 @@ fun MainScreen() {
                     NavigationBarItem(
                         selected = selectedItemPosition.value == index,
                         onClick = { selectedItemPosition.value = index },
-                        icon = { Icon(imageVector = navigationItem.icon, contentDescription = null) },
+                        icon = {
+                            Icon(
+                                imageVector = navigationItem.icon,
+                                contentDescription = null
+                            )
+                        },
                         label = {
                             Text(text = stringResource(id = navigationItem.titleResId))
                         },
